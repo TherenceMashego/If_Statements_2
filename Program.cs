9f47d1869778b3c46a4ea420309a9b8668a122da
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 16:28:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace If_Statements_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            return (num1 > num2) ? num1 : num2;
+        }
+    }
+}
